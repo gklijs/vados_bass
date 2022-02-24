a170e9f358585219df99a975e23c42562f5880f9
Gerard Klijs <g.klijs@gmail.com>
2022-02-24 15:32:52 +0100

    Update to dev version of vados. Change some colors.

--- generate.sh
+++ generate.sh
@@ -1,12 +1,13 @@
-git submodule update --init &&
-node-sass vados_bass.sass public/css/main.css &&
-CSS=public/css/main.css &&
-if [[ ! -f "$CSS" ]]; then
+CSS=public/css/main.min.css &&
+  if [[ ! -f "$CSS" ]]; then
     git submodule update --init &&
-    node-sass vados_bass.sass public/css/main.css
-fi &&
-BINARY=target/release/vados_bass &&
-if [[ ! -f "$BINARY" ]]; then
-    cargo build --release
-fi &&
-target/release/vados_bass
+      node-sass vados_bass.sass target/main.css &&
+      mkdir -p public/css &&
+      css-minify -f target/main.css -o public/css/
+  fi &&
+  BINARY=target/release/vados_bass &&
+  if [[ ! -f "$BINARY" ]]; then
+    rustup default stable &&
+      cargo build --release
+  fi &&
+  target/release/vados_bass
